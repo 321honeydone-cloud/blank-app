1716cd3371f93abfc351a0e30aec5c636f7f173f
321honeydone-cloud <321honeydone@gmail.com>
2026-01-30 21:18:34 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+DEFAULT_RATEBOOK_PATH = r"G:\My Drive\HoneyDone Propery Maintenance\HoneyDone_RateBook_Template_No_RoundUp.xlsx"
+DEFAULT_RATEBOOK_SHEET = "RateBook"
+
+TRIP_FEE = 100.00
+HOURLY_RATE = 100.00
+TAX_RATE = 0.06  # materials only
